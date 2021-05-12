ee1f86345704f0eb02f7abb0a68fea6b422397e4
Jatin Kumar <jatin.kumar@proplogix.com>
2021-05-12 16:30:10 -0400

    try faust driver

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 data "aws_eks_cluster" "cluster" {
-  name = "my-eks-cluster"
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
