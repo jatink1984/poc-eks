c52f57a4cdd466cbba8e752911cde49bb83e80ad
Jatin Kumar <jatin.kumar@proplogix.com>
2021-05-12 16:20:54 -0400

    try faust driver

--- main.tf
+++ main.tf
@@ -5,10 +5,6 @@ data "aws_eks_cluster" "cluster" {
   name = "my-eks-cluster"
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = "my-eks-cluster"
-}
-
 data "tls_certificate" "cert" {
   url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
